fd025ac6f48146343053deea40fe34143ccc54e5
Kuan Chien <kcyeo29@gmail.com>
2022-11-30 13:13:05 +0800

    Added card component to display event

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Card from "./components/Card/card";
 
 function App() {
   const [cities, setCities] = useState([]);
+  const [events, setEvents] = useState([]);
 
   const months = [
     "January",
@@ -30,12 +32,17 @@ function App() {
 
       const data = await response.json();
       const availableCities = data.map((v) => v.city);
-      const availableDates = data.map((v) => {
+      const events = data.map((v) => {
         const [day, month, year] = v.date.split(".");
 
-        return new Date(year, month - 1, day);
+        return {
+          ...v,
+          date: new Date(year, month - 1, day),
+        };
       });
       setCities(availableCities);
+
+      setEvents(events);
     })();
   }, []);
 
@@ -43,33 +50,39 @@ function App() {
     <div className="App">
       <h1>Event Listing</h1>
 
-      <label for="city">City </label>
-      <select
-        name="city"
-        id="city"
-        onChange={(event) => {
-          console.log("hello", event.target.value);
-        }}
-      >
-        {cities.map((v) => (
-          <option value={v}>{v}</option>
-        ))}
-      </select>
+      <div className="filter-container">
+        <label for="city">City </label>
+        <select
+          name="city"
+          id="city"
+          onChange={(event) => {
+            console.log("hello", event.target.value);
+          }}
+        >
+          {cities.map((v) => (
+            <option value={v}>{v}</option>
+          ))}
+        </select>
 
-      <label for="month">Month</label>
-      <select
-        name="month"
-        id="month"
-        onChange={(event) => {
-          console.log("hello", event.target.value);
-        }}
-      >
-        {months.map((v) => (
-          <option value={v}>{v}</option>
-        ))}
-      </select>
+        <label for="month">Month</label>
+        <select
+          name="month"
+          id="month"
+          onChange={(event) => {
+            console.log("hello", event.target.value);
+          }}
+        >
+          {months.map((v) => (
+            <option value={v}>{v}</option>
+          ))}
+        </select>
+      </div>
 
-      <div></div>
+      <div className="card-container">
+        {events.map((v) => {
+          return <Card eventItem={v} />;
+        })}
+      </div>
     </div>
   );
 }
